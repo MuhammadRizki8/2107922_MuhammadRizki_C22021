1edfcb793e90535be6d5b065a430ed2298e72a3f
MuhammadRizki8 <muhammadrizki@upi.edu>
2022-03-06 21:47:39 +0700

    ngotak-ngatik

--- 2107922_MuhammadRizki.php
+++ 2107922_MuhammadRizki.php
@@ -8,7 +8,6 @@
 			//Tugas Idividu membuat file berekstensi .php yang berisi nama dan NIM
 			echo "<p>Nama : Muhammad Rizki</p>";
 			echo "<p>NIM  : 2107922</p>";
-			echo "<p>Kelas: C2</p>";
 		?>
 	</body>
 </html>
